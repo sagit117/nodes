e916e857be0beba591692645b50acdb240c5ec95
axel <sagit117@gmail.com>
2024-07-12 08:56:35 +0300

    init

---
+++ src/main/java/ru/axel/Main.java
@@ -0,0 +1,22 @@
+package ru.axel;
+
+import ru.axel.nodes.Node;
+
+import java.util.function.BiConsumer;
+
+public class Main {
+    public static void main(String[] args) {
+        Node root = new Node() {};
+        Node child1 = new Node() {};
+        Node child2 = new Node() {};
+
+        root.addHandler("test", (o, node) -> System.out.println("init node: " + node.getClass().getTypeName()));
+
+        root.addChildNode(child1.addChildNode(child2));
+
+        child1.addHandler("test", (BiConsumer<Integer, Node>) (o, node) -> System.out.println(o + 1));
+
+        child2.emit("test", 2);
+        child1.emit("test", 1);
+    }
+}
---
+++ src/main/java/ru/axel/nodes/Node.java
@@ -0,0 +1,55 @@
+package ru.axel.nodes;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+
+abstract public class Node {
+    final private Set<Node> nodes = new HashSet<>();
+    private Node rootNode = null;
+    final private Map<String, Set<BiConsumer<Object, Node>>> handlers = new HashMap<>();
+
+    protected Node() {
+
+    }
+
+    public @NotNull Node addChildNode(@NotNull Node node) {
+        nodes.add(node.setRootNode(this));
+        return this;
+    }
+
+    private Node setRootNode(@NotNull Node node) {
+        rootNode = node;
+        return this;
+    }
+
+    public void emit(@NotNull String name, @Nullable Object data) {
+        emit(name, data, this);
+    }
+
+    public void emit(@NotNull String name) {
+        emit(name, null, this);
+    }
+
+    private void emit(@NotNull String name, @Nullable Object data, @NotNull Node node) {
+        if (handlers.containsKey(name)) {
+            handlers.get(name).forEach(biConsumer -> biConsumer.accept(data, node));
+        }
+
+        if (rootNode != null) rootNode.emit(name, data, node);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> void addHandler(@NotNull String name, BiConsumer<T, Node> biConsumer) {
+        if (handlers.containsKey(name)) {
+            handlers.get(name).add((BiConsumer<Object, Node>)biConsumer);
+        } else {
+            final Set<BiConsumer<Object, Node>> biConsumers = new HashSet<>();
+            biConsumers.add((BiConsumer<Object, Node>)biConsumer);
+
+            handlers.put(name, biConsumers);
+        }
+    }
+}
